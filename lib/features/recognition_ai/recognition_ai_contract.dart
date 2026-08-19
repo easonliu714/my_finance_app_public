f9b318014df07638cd6b2ce4a015c0b022f5537f
Liu Chia Yi <209346080+easonliu714@users.noreply.github.com>
2026-08-19 10:57:06 +0800

    feat: add shared recognition AI session contract

---
+++ lib/features/recognition_ai/recognition_ai_contract.dart
@@ -0,0 +1,77 @@
+enum RecognitionAiFallbackReason {
+  none,
+  quotaExhausted,
+  authenticationFailed,
+  modelUnavailable,
+  serviceUnavailable,
+  timeout,
+  network,
+}
+
+class RecognitionAiRoutingEvent {
+  const RecognitionAiRoutingEvent({
+    required this.keyGroupAlias,
+    required this.model,
+    required this.reason,
+    required this.physicalRequestSent,
+    this.message = '',
+  });
+
+  final String keyGroupAlias;
+  final String model;
+  final RecognitionAiFallbackReason reason;
+  final bool physicalRequestSent;
+  final String message;
+
+  Map<String, Object?> toSafeJson() => <String, Object?>{
+        'key_group_alias': keyGroupAlias,
+        'model': model,
+        'reason': reason.name,
+        'physical_request_sent': physicalRequestSent,
+        if (message.isNotEmpty) 'message': message,
+      };
+}
+
+class RecognitionSessionContext {
+  const RecognitionSessionContext({
+    required this.logicalInvocationId,
+    required this.provider,
+    required this.activeModel,
+    required this.keyGroupAlias,
+    required this.logicalInvocationCount,
+    required this.physicalAttemptCount,
+    required this.modelAttemptCount,
+    required this.keyGroupAttemptCount,
+    required this.fallbackReason,
+    required this.modelCatalogChecked,
+    this.events = const <RecognitionAiRoutingEvent>[],
+  });
+
+  final String logicalInvocationId;
+  final String provider;
+  final String activeModel;
+  final String keyGroupAlias;
+  final int logicalInvocationCount;
+  final int physicalAttemptCount;
+  final int modelAttemptCount;
+  final int keyGroupAttemptCount;
+  final RecognitionAiFallbackReason fallbackReason;
+  final bool modelCatalogChecked;
+  final List<RecognitionAiRoutingEvent> events;
+
+  Map<String, Object?> toSafeJson() => <String, Object?>{
+        'provider': provider,
+        'logical_invocation_id': logicalInvocationId,
+        'active_model': activeModel,
+        'key_group_alias': keyGroupAlias,
+        'logical_invocation_count': logicalInvocationCount,
+        'physical_attempt_count': physicalAttemptCount,
+        'model_attempt_count': modelAttemptCount,
+        'key_group_attempt_count': keyGroupAttemptCount,
+        'fallback_reason': fallbackReason.name,
+        'model_catalog_checked': modelCatalogChecked,
+        'routing_events': <Object?>[
+          for (final event in events) event.toSafeJson(),
+        ],
+      };
+}
