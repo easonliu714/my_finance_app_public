26573ebc3ecce01a4b6de137c8f569cc10f78804
Liu Chia Yi <209346080+easonliu714@users.noreply.github.com>
2026-08-19 14:47:42 +0800

    test: align P4.19.1 version contract 437

--- test/p4_12_34_1_version_contract_test.dart
+++ test/p4_12_34_1_version_contract_test.dart
@@ -6,7 +6,7 @@ import 'package:my_finance_app/features/invoice/lab/private_cloud_invoice_lab_co
 void main() {
   test('package and validation versions stay aligned', () {
     final pubspec = File('pubspec.yaml').readAsStringSync();
-    expect(pubspec, contains('version: 4.19.1+436'));
-    expect(PrivateCloudInvoiceLabConfig.validationVersion, '4.19.1+436');
+    expect(pubspec, contains('version: 4.19.1+437'));
+    expect(PrivateCloudInvoiceLabConfig.validationVersion, '4.19.1+437');
   });
 }
