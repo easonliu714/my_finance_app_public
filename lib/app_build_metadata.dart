3ce40a6c4e0a95f37d4d896f336e8e3fc4f61aed
Liu Chia Yi <209346080+easonliu714@users.noreply.github.com>
2026-09-11 21:01:33 +0800

    release: align app metadata to build 461

--- lib/app_build_metadata.dart
+++ lib/app_build_metadata.dart
@@ -3,6 +3,6 @@ class AppBuildMetadata {
 
   static const String appName = 'my_finance_app';
   static const String appDisplayName = 'My Finance App';
-  static const String appVersion = '4.20.4+460';
-  static const String phase = 'P4.20.4-merchant-decision-composer-460';
+  static const String appVersion = '4.20.4+461';
+  static const String phase = 'P4.20.4-merchant-decision-composer-hotfix-461';
 }
